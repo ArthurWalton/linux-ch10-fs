3761ff55303546a240549c40ec44601e144f1029
Arthur Walton <arthur.walton2@gmail.com>
2014-05-03 14:39:31 -0700

    Added copyright message to header

--- ch10fs.h
+++ ch10fs.h
@@ -1,3 +1,13 @@
+/* Ch10Fs header
+ *
+ * Copyright © 2007 Arthur Walton (arthur.walton2@gmail.com)
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
 #ifndef __LINUX_CH10FS_FS_H
 #define __LINUX_CH10FS_FS_H
 
